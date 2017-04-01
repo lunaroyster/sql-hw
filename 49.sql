604af4fe0501dd420e760780e5332562f844cd5d
lunaroyster <oneinaquadrillion@live.com>
2017-04-01 19:48:13 +0000

    48; 49; 53

---
+++ 49.sql
@@ -0,0 +1,2 @@
+INSERT INTO Employees(id, name, designation, employeeID, joiningDate, DOB)
+VALUES (19, "Daya Kishore", "Operator", "P003", "19-JAN-2008", "11-JUL-1984")
---
+++ 53.sql
@@ -0,0 +1,2 @@
+INSERT INTO Arrivals (orderID, name, pType, dateOfOrder, cost, quantity)
+VALUES (14, "Velvet touch", "Double Bed", #03/25/03#, 25000, 30)
